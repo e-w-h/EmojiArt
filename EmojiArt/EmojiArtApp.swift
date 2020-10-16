b5a20ddd82f9494bee981507db79e1780c97de96
Eric Hou <erichou24@gmail.com>
2020-10-16 15:58:57 -0400

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Eric Hou on 10/16/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
