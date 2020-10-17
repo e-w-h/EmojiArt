3c3152889625dc953a592b558fc2000cd7f8a81a
Eric Hou <erichou24@gmail.com>
2020-10-17 19:20:02 -0400

    sidebar for emojis

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -11,7 +11,13 @@ struct EmojiArtDocumentView: View {
     @ObservedObject var document: EmojiArtDocument
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            // Using an identifiable so that ForEach can iterate through the array
+            // Everything in Swift has an invisible var called self
+            // Swift has syntax called keypath that specifies the var on the object using backslash
+            ForEach(EmojiArtDocument.palette.map { String($0) }, id: \.self) { emoji in
+                Text(emoji)
+            }
+        }
     }
 }
