4eca6134de6128bd663850ca31aebd387c282238
Eric Hou <erichou24@gmail.com>
2020-10-18 23:52:22 -0400

    protected init

--- EmojiArt/EmojiArt.swift
+++ EmojiArt/EmojiArt.swift
@@ -19,6 +19,18 @@ struct EmojiArt {
         var size: Int
         // Privately managed public variable
         let id: Int
+        
+        // In case someone wants to creates an emoji and sets the id themselves
+        // The custom init has all the standard boiler you get from a free init
+        // Making it private nobody can create an emoji besides Emoji
+        // File private makes the function private in the file and lets EmojiArt use the addEmoji func
+        fileprivate init(text: String, x : Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
     }
     
     private var uniqueEmojiId = 0
