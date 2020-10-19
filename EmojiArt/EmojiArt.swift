52c072a28a876e1a05d429de84bf45d1f58fa426
Eric Hou <erichou24@gmail.com>
2020-10-18 23:34:34 -0400

    parameters and "objects" of the model

--- EmojiArt/EmojiArt.swift
+++ EmojiArt/EmojiArt.swift
@@ -8,4 +8,19 @@
 import Foundation
 
 struct EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    // Want to use ForEach so we add Identifiable
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int
+        var y: Int
+        var size: Int
+        // Privately managed public variable
+        let id: Int
+    }
+    
+    private var uniqueEmojiId = 0
+    
 }
