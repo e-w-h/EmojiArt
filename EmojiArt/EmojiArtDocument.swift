7e5192ad9523b5ee9ec97d4fe3a35dd22bd59a24
Eric Hou <erichou24@gmail.com>
2020-10-22 10:03:37 -0400

    intent funds

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -12,6 +12,20 @@ class EmojiArtDocument: ObservableObject {
     static let palette: String = "⭐️🌧🍎"
     
     // Published so that whenever something changes the ObservableObject can redraw
-    // Private because viewmodel interprets the view 
+    // Private because viewmodel interprets the view
     @Published private var emojiArt: EmojiArt = EmojiArt()
+    
+    // MARK: - Intent(s)
+    
+    func addEmoji(_ emoji: String, at location: CGPoint, size: CGFloat) {
+        
+    }
+    
+    func moveEmoji(_ emoji: EmojiArt.Emoji, by offset: CGSize) {
+        
+    }
+    
+    func scaleEmoji(_ emoji: EmojiArt.Emoji, by scale: CGFloat) {
+        
+    }
 }
