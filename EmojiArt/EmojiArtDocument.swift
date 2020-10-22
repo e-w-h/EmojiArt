8c05b43b68f8d81b27e9f87972836d053d86e150
Eric Hou <erichou24@gmail.com>
2020-10-22 09:54:46 -0400

    create an emoji art model

--- EmojiArt/EmojiArtDocument.swift
+++ EmojiArt/EmojiArtDocument.swift
@@ -8,5 +8,10 @@
 import SwiftUI
 
 class EmojiArtDocument: ObservableObject {
+    
     static let palette: String = "⭐️🌧🍎"
+    
+    // Published so that whenever something changes the ObservableObject can redraw
+    // Private because viewmodel interprets the view 
+    @Published private var emojiArt: EmojiArt = EmojiArt()
 }
