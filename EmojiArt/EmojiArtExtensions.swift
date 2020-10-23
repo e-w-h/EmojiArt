f662dc048941b7ac59a7c110fd677757be59f9bc
Eric Hou <erichou24@gmail.com>
2020-10-23 08:55:23 -0400

    first index and contains

--- EmojiArt/EmojiArtExtensions.swift
+++ EmojiArt/EmojiArtExtensions.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 extension Collection where Element: Identifiable {
-    
+    func firstIndex(matching element: Element) -> Index? {
+        firstIndex(where: {$0.id == element.id })
+    }
+    func contains(matching element: Element) -> Bool {
+        contains(where: {$0.id == element.id })
+    }
 }
 
