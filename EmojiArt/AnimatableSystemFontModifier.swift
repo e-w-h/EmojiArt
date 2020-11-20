c80c1bb54b555d5c8e874635b77250d59ef5af6d
Eric Hou <erichou24@gmail.com>
2020-11-20 08:23:54 -0500

    animatable font modifier file

---
+++ EmojiArt/AnimatableSystemFontModifier.swift
@@ -0,0 +1,16 @@
+//
+//  AnimatableSystemFontModifier.swift
+//  EmojiArt
+//
+//  Created by Eric Hou on 11/20/20.
+//
+
+import SwiftUI
+
+// ViewModifer to set the font to the system font of a given size
+// (and weight and design)
+// Changes to the size can be animated
+
+struct AnimatableSystemFontModifier: AnimatableModifier {
+
+}
